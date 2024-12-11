bca6d99dbceca37215a3c297be04f1de1e5a0414
anm7 <nsite2014@gmail.com>
2024-12-11 13:19:13 +0200

    api

--- lib/service.ts
+++ lib/service.ts
@@ -1,10 +1,36 @@
 import { fetchAPI } from "./base";
 
 
+// export async function getBlogs() {
+//   const data = await fetchAPI(
+//     `query FetchPosts {
+//       category(id: "dGVybToxMQ==") {
+//         posts(first: 100, where: {orderby: {field: DATE, order: DESC}}) {
+//           nodes {
+//             excerpt
+//             featuredImage {
+//               node {
+//                 sourceUrl
+//               }
+//             }
+//             slug
+//             title
+//             content
+//               url {
+//               projectLink
+//             }
+//           }
+//         }
+//       }
+//     }`,
+//   );
+//   return data?.category?.posts?.nodes;
+// }
+
 export async function getPosts() {
   const data = await fetchAPI(
     `query FetchPosts {
-        posts {
+        posts(first: 100) {
           nodes {
             excerpt
             featuredImage {
@@ -14,37 +40,16 @@ export async function getPosts() {
             }
             slug
             title
+            content
           }
         }
-      }`,
-    // {
-      // variables: {
-        // first,
-      // },
-    // }
-    // `query NewQuery {
-    //   category(id: "dGVybToz") {
-    //     id
-    //     posts {
-    //       nodes {
-    //         excerpt
-    //         featuredImage {
-    //           node {
-    //             sourceUrl
-    //           }
-    //         }
-    //         slug
-    //         title
-    //       }
-    //     }
-    //   }
-    // }`
+      
+    }`,
   );
-
   return data?.posts?.nodes;
 }
 
-export async function getPostBySlug(slug: string) {
+export async function getPostBySlug(postSlug: string) {
   const data = await fetchAPI(
     `query GetPost($id: ID = "") {
     post(id: $id, idType: SLUG) {
@@ -60,7 +65,7 @@ export async function getPostBySlug(slug: string) {
   }`,
     {
       variables: {
-        id: slug,
+        id: postSlug,
       },
     }
   );
--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "avto.powweb.space",
+          },
+          {
+            protocol: "https",
+            hostname: "powweb.space",
+          },
+          {
+            protocol: "https",
+            hostname: "i0.wp.com",
+          },
+          {
+            protocol: "https",
+            hostname: "i.postimg.cc",
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
