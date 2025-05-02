203edc348c30d1e4a2f25a398bbc3f2ba1da8b2e
anm7 <nsite2014@gmail.com>
2025-05-02 22:56:10 +0300

    count

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,7 @@ export default function RootLayout({
             </footer>
           </div>
         </Providers>
+        <script data-host="https://count.bg" data-dnt="false" src="https://count.bg/js/script.js" id="ZwSg9rf6GA" async defer></script>
       </body>
     </html>
   );
